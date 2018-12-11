d368baf2f53a8c0e63d9f86f9b8fc980255bda9a
mickname <mikko.luodemaa@mikkis.net>
2018-12-11 19:28:28 +0200

    Day 11 p2: Micro-optimized inner loop for over 50% reduction in run time, at the cost of readability.

--- aoc11.2.py
+++ aoc11.2.py
@@ -16,18 +16,6 @@ def cell_power(x, y, serial):
     return hundreds - 5
 
 
-def ii_sum(ii, x, y, s):
-    '''Read the sum of an integral image square x, y size z.
-    '''
-
-    return (
-        ii[H * (y + s) + x + s]  # bottom right
-        + ii[H * y + x]          # top left
-        - ii[H * y + x + s]      # top right
-        - ii[H * (y + s) + x]    # bottom left
-    )
-
-
 def main():
     serial = int(sys.argv[1])
 
@@ -43,15 +31,24 @@ def main():
         top_left = integral[H * (y - 1) + x - 1]
         integral[H * y + x] = grid[H * (y - 1) + x - 1] + left + top - top_left
 
-    # Find nxn square with largest sum
+    # Find sxs square with largest sum. See earlier commits for a more readable
+    # version.
     largest_total = float('-inf')
     largest_pos = None
-    for grid_size in range(1, 300):
-        for y, x in itertools.product(range(0, H - grid_size - 1), range(0, W - grid_size - 1)):
-            total = ii_sum(integral, x, y, grid_size)
-            if total > largest_total:
-                largest_total = total
-                largest_pos = (x + 1, y + 1, grid_size)
+    for s in range(1, 300):
+        for y in range(0, H - s - 1):
+            Hy = H * y
+            Hys = H * (y + s)
+            Hy_s = Hy + s
+            Hys_s = Hys + s
+            for x in range(0, W - s - 1):
+                total = (
+                    integral[Hys_s + x] + integral[Hy + x]
+                    - integral[Hy_s + x] - integral[Hys + x]
+                )
+                if total > largest_total:
+                    largest_total = total
+                    largest_pos = (x + 1, y + 1, s)
 
     print("Largest total power in {},{},{} with power = {}".format(*largest_pos, largest_total))
 
