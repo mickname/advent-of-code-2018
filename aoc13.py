ac4a2d002920d427dd45883aa47ad30693a2afa5
mickname <mikko.luodemaa@mikkis.net>
2018-12-16 15:18:06 +0200

    A working solution for day 13 p1.

--- aoc13.py
+++ aoc13.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import sys
+import collections
+from operator import itemgetter
 
+
+Cart = collections.namedtuple('Cart', ['x', 'y', 'symbol', 'dx', 'dy', 'turn'])
 
 CART_TO_TRACK = {
     '<': '-',
@@ -10,19 +14,35 @@ CART_TO_TRACK = {
 }
 
 CART_TO_DIR = {
-    '<': (-1, 0),
+    '^': (0, -1),
     '>': (1, 0),
-    '^': (0, 1),
-    'v': (0, -1),
+    'v': (0, 1),
+    '<': (-1, 0),
 }
+DIR_TO_CART = {dir: symbol for symbol, dir in CART_TO_DIR.items()}
 
 
 def print_state(grid, w, h, carts):
     grid_with_carts = grid[:]
-    for y, x, symbol in carts:
-        grid_with_carts[w * y + x] = symbol
+    for c in carts:
+        grid_with_carts[w * c.y + c.x] = c.symbol
     print('\n'.join([''.join(grid_with_carts[w * row:w * row + w])
           for row in range(h)]))
+
+
+def new_dir(cart, track):
+    if track == '+':
+        dirs = ['^', '>', 'v', '<']
+        new_symbol = dirs[(dirs.index(cart.symbol) + cart.turn) % 4]
+        return (new_symbol, *CART_TO_DIR[new_symbol])
+    elif track == '\\':
+        dx = cart.dy
+        dy = cart.dx
+    elif track == '/':
+        dx = -cart.dy
+        dy = -cart.dx
+
+    return (DIR_TO_CART[dx, dy], dx, dy)
 
 
 def main():
@@ -38,7 +58,7 @@ def main():
                     continue
                 try:
                     grid.append(CART_TO_TRACK[char])
-                    carts.append((row, col, char))
+                    carts.append(Cart(col, row, char, *CART_TO_DIR[char], -1))
                 except KeyError:
                     grid.append(char)
 
@@ -47,17 +67,42 @@ def main():
 
     print("Grid size: {}, {}".format(width, height))
 
-    for t in range(2):
+    t = 0
+    collision = False
+    while not collision:
         print("tick = {}".format(t))
         # Print grid
         print_state(grid, width, height, carts)
         print('')
+        t += 1
         # Update cart locations
-        carts.sort()
+        carts.sort(key=itemgetter(1, 0))
         for i, cart in enumerate(carts):
-            pos = cart[0:2]
-            vec = CART_TO_DIR(cart[2])
-            new_pos = (pos[0] + vec[0], pos[1] + vec[1])
+            x = cart.x + cart.dx
+            y = cart.y + cart.dy
+            # Check for collisions:
+            for cart2 in carts:
+                if x == cart2.x and y == cart2.y:
+                    collision = True
+                    break
+            if collision:
+                break
+
+            symbol = cart.symbol
+            dx = cart.dx
+            dy = cart.dy
+            turn = cart.turn
+
+            at_pos = grid[width * y + x]
+            if at_pos in ('/', '\\'):
+                symbol, dx, dy = new_dir(cart, at_pos)
+            if at_pos == '+':
+                symbol, dx, dy = new_dir(cart, at_pos)
+                turn = cart.turn + 1 if cart.turn < 1 else -1
+
+            carts[i] = Cart(x, y, symbol, dx, dy, turn)
+
+    print("Collision at x,y = {},{}".format(x, y))
 
 
 if __name__ == '__main__':
